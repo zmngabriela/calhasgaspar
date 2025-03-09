2aae639e59707d98a6c26551b1d8a62d8e5fae35
zmngabriela <zmngabriela@gmail.com>
2025-03-09 21:24:43 +0100

    fix: fixing wrong number on whatsapp absolute icon

--- src/components/Whatsapp.vue
+++ src/components/Whatsapp.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="fixed bottom-12 right-12 bg-none animate-pulse">
-        <a href="https://api.whatsapp.com/send?phone=+55999797075" target="_blank">
+        <a href="https://api.whatsapp.com/send?phone=+5547999797075" target="_blank">
             <img src="../assets/icons/whatsapp-icon.png" class="size-[40px] hover:scale-105" alt="">
         </a>
     </div>
